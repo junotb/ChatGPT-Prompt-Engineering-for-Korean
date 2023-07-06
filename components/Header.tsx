e2c4546646f0f0c26dc2f7b2641f1f3b4292757f
jhlee <jhlee@spicusinc.com>
2023-07-06 15:37:04 +0900

    draw layouts

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex justify-between px-2 landscape:px-4 min-h-[4rem] bg-[rgb(var(--background-rgb))] items-center">
-      <Link
-        href="https://learn.deeplearning.ai/"
-        target="_blank"
-        className="w-8 h-8 bg-contain bg-no-repeat bg-[url('/openai-logomark.svg')] dark:bg-[url('/openai-white-logomark.svg')]"
-      ></Link>
+    <div className="flex justify-start px-2 landscape:px-4 min-h-[4rem] bg-[rgb(var(--background-rgb))] items-center">
       <Link
         href={"/"}
       ><strong>ChatGPT Prompt Engineering for Korean</strong></Link>
